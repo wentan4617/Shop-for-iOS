3dad3b4afd6aa5a811dd80a93e8596bf2b26a65f
tony <908601756@qq.com>
2016-03-14 13:23:56 +0800

    解决真机访问找不到Link libraries的问题

--- ygcr/TabHomeController.m
+++ ygcr/TabHomeController.m
@@ -16,8 +16,9 @@
 #import "CategoryEntity.h"
 #import "ProductEntity.h"
 #import "TabHomeRightTableCell.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface TabHomeController () <UITableViewDataSource, UITableViewDelegate>
+@interface TabHomeController () <UITableViewDataSource, UITableViewDelegate, CLLocationManagerDelegate>
 {
     UITableView *_leftTable;
     
@@ -28,6 +29,8 @@
     NSInteger _leftTableCurRow; //当前被选中的行
     
     NSArray *_categories;
+    
+    CLLocationManager *_locationManager;
 }
 @end
 
@@ -69,6 +72,27 @@
     _rightTable.delegate = self;
     
     [self getData];
+    
+    //定位管理器
+    _locationManager = [[CLLocationManager alloc] init];
+    if (![CLLocationManager locationServicesEnabled]) {
+        NSLog(@"定位服务未打开");
+    }
+    //如果没有授权则请求用户授权
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
+        [_locationManager requestWhenInUseAuthorization];
+    }
+    //已授权则设置定位参数
+    if ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorized) {
+        _locationManager.delegate = self;
+        //设置定位精度
+        _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        //定位频率,每隔十米定位一次
+        CLLocationDistance distance = 10.0;
+        _locationManager.distanceFilter = distance;
+        //启动跟踪定位
+        [_locationManager startUpdatingLocation];
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -222,6 +246,19 @@
 - (void)doClickNetworkBrokenView
 {
     [self getData];
+}
+
+
+#pragma mark - CLLocationManagerDelegate 代理
+
+#pragma mark 跟踪定位代理方法，每次位置发生变化即会执行（只要定位到相应位置）
+//可以通过模拟器设置一个虚拟位置，否则在模拟器中无法调用此方法
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
+    CLLocation *location=[locations firstObject];//取出第一个位置
+    CLLocationCoordinate2D coordinate=location.coordinate;//位置坐标
+    NSLog(@"经度：%f,纬度：%f,海拔：%f,航向：%f,行走速度：%f",coordinate.longitude,coordinate.latitude,location.altitude,location.course,location.speed);
+    //如果不需要实时定位，使用完即使关闭定位服务
+    [_locationManager stopUpdatingLocation];
 }
 
 @end
